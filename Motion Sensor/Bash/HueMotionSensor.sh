94191b8164a22e7ad04a0929946ace65a3d2e33c
Thomas Krampe <thomas.krampe@myctx.net>
2017-08-05 13:52:31 +0200

    Update HueMotionSensor.sh

--- Motion Sensor/Bash/HueMotionSensor.sh
+++ Motion Sensor/Bash/HueMotionSensor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Make sure curl is installed on your system
+
 MOTION=$(curl -s –request GET http://<IP-Adresse-der-Bridge>/api/<API-User>/sensors/11/)
 
 if [ "${MOTION:21:4}" = "true" ]; then
     curl -s –request POST https://maker.ifttt.com/trigger/<Action>/with/key/<API-Key>
-    echo “Bewegung erkannt.”
+    echo “Motion detected.”
 else
-    echo “Keine Bewegung erkannt.”
+    echo “No motion detected.”
 fi
